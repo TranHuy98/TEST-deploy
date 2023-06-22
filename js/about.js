1ea7645a58abe2a9949216e7300855d75636465c
TranHuy98 <huy8663@gmail.com>
2023-06-22 20:33:48 +0700

    test14

--- js/about.js
+++ js/about.js
@@ -1,7 +1,10 @@
 // manager detail
 
-$('.manager-card').click(function(){
-    let imgLink = $('this').find('.manager-image');
-    console.log(imgLink);
+// https://stackoverflow.com/questions/1474089/how-to-select-a-single-child-element-using-jquery
 
-});
+let test1 = document.getElementById('test1');
+
+let img = test1.getElementsByTagName('manager-image');
+
+console.log(test1);
+console.log(img);
